df75b2d16d97beca34aa1956fe0cbcb9a06ab7b3
OmarMWarraich <earthling986@gmail.com>
2022-12-23 23:33:23 +0500

    feat: Create Custom Methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,10 @@ class Like < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
   after_save :update_likes_counter
 
+  private
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: :author_id
   has_many :comments, dependent: :destroy, foreign_key: :post_id
   has_many :likes, dependent: :destroy, foreign_key: :post_id
+  after_save :update_posts_counter
+
+  def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
+
 end
