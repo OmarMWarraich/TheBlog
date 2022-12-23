a2d3f953fcda1bea9315626ae1160f3857b8b746
OmarMWarraich <earthling986@gmail.com>
2022-12-23 23:16:30 +0500

    feat: Update Associations between Model Classes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  after_save :update_likes_counter
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  belongs_to :user, class_name: 'User', foreign_key: :author_id
+  has_many :comments, dependent: :destroy, foreign_key: :post_id
+  has_many :likes, dependent: :destroy, foreign_key: :post_id
 end
